70baa1a64eaa27875ea662c3f3feeb99cb6aed38
Andrey Dremin <sankoa230@gmail.com>
2024-12-23 23:22:52 +0300

    one more change

--- src/App.tsx
+++ src/App.tsx
@@ -1,100 +1,19 @@
 import { useEffect, useState } from "react";
 import { useInView } from "react-intersection-observer";
 import "./App.css";
+import { data } from "./constants";
 
-interface IItem {
+
+export interface IItem {
   title: string;
 }
 
-type Items = IItem[];
+export type Items = IItem[];
 
 function App() {
   const LIMIT = 1;
 
   const { ref: refInView, inView } = useInView();
-
-  const data: Items = [
-    { title: "Home" },
-    { title: "Work" },
-    { title: "Play" },
-    { title: "Learn" },
-    { title: "Read" },
-    { title: "Write" },
-    { title: "Code" },
-    { title: "Design" },
-    { title: "Plan" },
-    { title: "Build" },
-    { title: "Explore" },
-    { title: "Travel" },
-    { title: "Dream" },
-    { title: "Grow" },
-    { title: "Reflect" },
-    { title: "Relax" },
-    { title: "Focus" },
-    { title: "Connect" },
-    { title: "Share" },
-    { title: "Think" },
-    { title: "Help" },
-    { title: "Give" },
-    { title: "Learn" },
-    { title: "Teach" },
-    { title: "Discover" },
-    { title: "Create" },
-    { title: "Inspire" },
-    { title: "Solve" },
-    { title: "Play" },
-    { title: "Work" },
-    { title: "Act" },
-    { title: "Rest" },
-    { title: "Start" },
-    { title: "Finish" },
-    { title: "Lead" },
-    { title: "Follow" },
-    { title: "Adapt" },
-    { title: "Invent" },
-    { title: "Smile" },
-    { title: "Laugh" },
-    { title: "Love" },
-    { title: "Care" },
-    { title: "Run" },
-    { title: "Jump" },
-    { title: "Walk" },
-    { title: "Climb" },
-    { title: "Think" },
-    { title: "Read" },
-    { title: "Write" },
-    { title: "Grow" },
-    { title: "Learn" },
-    { title: "Explore" },
-    { title: "Dream" },
-    { title: "Plan" },
-    { title: "Build" },
-    { title: "Reflect" },
-    { title: "Relax" },
-    { title: "Focus" },
-    { title: "Connect" },
-    { title: "Share" },
-    { title: "Discover" },
-    { title: "Inspire" },
-    { title: "Create" },
-    { title: "Help" },
-    { title: "Lead" },
-    { title: "Follow" },
-    { title: "Adapt" },
-    { title: "Invent" },
-    { title: "Solve" },
-    { title: "Act" },
-    { title: "Smile" },
-    { title: "Laugh" },
-    { title: "Love" },
-    { title: "Care" },
-    { title: "Rest" },
-    { title: "Run" },
-    { title: "Jump" },
-    { title: "Walk" },
-    { title: "Climb" },
-  ];
-  
 
   const [currentArrays, setCurrentArrays] = useState<Items[]>([]);
 
